232155e11b552d3302317a515d8dab10e4c5fd8c
Alena Karenina <karena1q84@gmail.com>
2023-10-01 15:39:17 +0500

    feat: добавит тип MockServerData

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,3 +4,4 @@ export * from './cities.enum.js';
 export * from './houses-types.enum.js';
 export * from './user.type.js';
 export * from './user-types.enum.js';
+export * from './mock-server-data.type.js';
---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,15 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  previewImages: string[];
+  placeImages: string[];
+  cities: string[];
+  housesTypes: string[];
+  conveniences: string[];
+  usernames: string[];
+  emails: string[];
+  avatars: string[];
+  passwords: string[];
+  userTypes: string[];
+  coordinates: string[];
+}
