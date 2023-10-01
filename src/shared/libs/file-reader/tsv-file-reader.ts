a9731303e7509f819909c7c679f67993a8c09556
Alena Karenina <karena1q84@gmail.com>
2023-10-01 18:34:50 +0500

    feat: рефакторит класс TSVFileReader. WIP

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,17 +1,15 @@
 import {FileReader} from './file-reader.interface.js';
 import {readFileSync} from 'node:fs';
+import EventEmitter from 'node:events';
 
-export class TSVFilerReader implements FileReader {
-  private rawData = '';
-
-  constructor(
-    private readonly filename: string,
-  ) {
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
   }
 
   public read(): void {
     try {
-      this.rawData = readFileSync(this.filename, 'utf-8');
+      // Код для работы с потоками
     } catch (error: unknown) {
       console.error(`Can't read file from path ${this.filename}.`);
 
