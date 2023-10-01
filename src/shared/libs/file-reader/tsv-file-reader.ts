740e0b8cac5b062c36102d8320d5efafbb41b50d
Alena Karenina <karena1q84@gmail.com>
2023-10-01 18:32:45 +0500

    feat: рефакторинг, функция createOffer. WIP

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,6 +1,5 @@
 import {FileReader} from './file-reader.interface.js';
 import {readFileSync} from 'node:fs';
-import {Cities, Conveniences, Offer, HousesTypes, UserTypes} from '../../types/index.js';
 
 export class TSVFilerReader implements FileReader {
   private rawData = '';
@@ -20,63 +19,5 @@ export class TSVFilerReader implements FileReader {
         console.error(error.message);
       }
     }
-  }
-
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      throw new Error('File was not read');
-    }
-
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim().length > 0)
-      .map((line) => line.split('\t'))
-      .map(([
-        name,
-        description,
-        date,
-        city,
-        previewImage,
-        placeImages,
-        isPremium,
-        isFavorite,
-        rating,
-        type,
-        roomsAmount,
-        guestsAmount,
-        price,
-        conveniences,
-        authorName,
-        email,
-        avatarUrl,
-        password,
-        userType,
-        latitude,
-        longitude]) =>
-        ({
-          name,
-          description,
-          date,
-          city: city as Cities,
-          previewImage,
-          placeImages: placeImages.split(';'),
-          isPremium: Boolean(isPremium),
-          isFavorite: Boolean(isFavorite),
-          rating: Number(rating),
-          type: type as HousesTypes,
-          roomsAmount: Number(roomsAmount),
-          guestsAmount: Number(guestsAmount),
-          price: Number(price),
-          conveniences: conveniences.split(';') as Conveniences[],
-          author: {
-            name: authorName,
-            email,
-            avatarUrl,
-            password,
-            type: userType as UserTypes,
-          },
-          cityCoordinates: {latitude: Number(latitude), longitude: Number(longitude)},
-        })
-      );
   }
 }
